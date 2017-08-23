8cd964b3df4765bb57e675f4bc255ed57b282cfe
ale5000 <ale5000-git@users.noreply.github.com>
2017-08-23 03:07:33 +0200

    Updated compatibility layer

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ def init():
         os.system("TITLE "+__app__)
 
     # Activate Python compatibility layer
-    pycompatlayer.set_utf8_default()
+    pycompatlayer.set_default_encoding()
     pycompatlayer.fix_all()
 
     # Add tools folder to search path (used from subprocess)
